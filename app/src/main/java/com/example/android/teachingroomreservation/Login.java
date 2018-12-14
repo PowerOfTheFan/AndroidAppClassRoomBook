02764dd19f5124f0de9eaf881233986254e6dcf1
Truongtansang <32859943+Truongtansang@users.noreply.github.com>
2018-12-14 16:38:46 +0700

    erererrre

--- app/src/main/java/com/example/android/teachingroomreservation/Login.java
+++ app/src/main/java/com/example/android/teachingroomreservation/Login.java
@@ -1,7 +1,10 @@
 package com.example.android.teachingroomreservation;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +13,33 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.buttonPanelID:
+                break;
+            case R.id.buttonPanelIDout:
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.exit_captione)
+                        .setMessage(R.string.exit_mess)
+                        .setNegativeButton(R.string.Button_yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setPositiveButton(R.string.Button_no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                return;
+                            }
+                        })
+                        .show();
+                break;
+        }
+        return true;
+    }
+
 }
